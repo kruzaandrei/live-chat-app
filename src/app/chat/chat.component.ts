57305823c364b8576fe4265ff445c852610ade84
Kruza Andrei <kruzaandrei@gmail.com>
2024-10-07 11:04:53 +0300

    feat: added required material modules to Chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
