708f9907b888bf9b78c468fb321593df8e4b0390
Kruza Andrei <kruzaandrei@gmail.com>
2024-10-07 10:30:58 +0300

    feat: generated components

---
+++ src/app/username-dialog/username-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './username-dialog.component.html',
+  styleUrl: './username-dialog.component.scss'
+})
+export class UsernameDialogComponent {
+
+}
