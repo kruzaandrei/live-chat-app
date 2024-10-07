7a4804a035f1cc821320413f89906a6c6e9bd343
Kruza Andrei <kruzaandrei@gmail.com>
2024-10-07 10:41:23 +0300

    feat: added mat dialog for username input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { UsernameDialogComponent } from './username-dialog/username-dialog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule, MatDialogModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private dialog: MatDialog) { }
+
+  ngOnInit() {
+    const dialogRef = this.dialog.open(UsernameDialogComponent, {
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((username: string) => {
+    });
+  }
 }
--- src/app/username-dialog/username-dialog.component.ts
+++ src/app/username-dialog/username-dialog.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MatDialogRef, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-username-dialog',
-  standalone: true,
-  imports: [],
   templateUrl: './username-dialog.component.html',
-  styleUrl: './username-dialog.component.scss'
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ],
 })
 export class UsernameDialogComponent {
+  username: string = '';
 
+  constructor(private dialogRef: MatDialogRef<UsernameDialogComponent>) { }
+
+  submit() {
+    if (this.username.trim()) {
+      this.dialogRef.close(this.username.trim());
+    }
+  }
 }
