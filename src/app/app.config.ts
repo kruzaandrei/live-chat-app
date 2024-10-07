6e8c2e79a10cd90f0df30cc4bada33616821b1f7
Kruza Andrei <kruzaandrei@gmail.com>
2024-10-07 13:25:17 +0300

    feat: added imports to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,21 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideStore } from '@ngrx/store';
+import { userReducer } from './store/user/user.reducer';
+import { messageReducer } from './store/message/message.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { MessageEffects } from './store/message/message.effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideStore({ user: userReducer, messages: messageReducer }),
+    importProvidersFrom(
+      EffectsModule.forRoot([MessageEffects]))
+  ]
 };
